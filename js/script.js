4ac8302705e4b21dc0cecec8b9fdc84f1478123c
Daniel Filipe <touristicmite10@gmail.com>
2022-05-18 19:13:59 -0300

    Changes in some pars of the code and X and Y movimentation on the ball

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 $(document).ready(() => {
 
     /** ELEMENTS THAT WILL BE USED IN THE GAMEPLAY PART */
+    var player = $("#player")
+    var oponent = $("#oponent")
+    var ball = $("#ball")
+    var border_bottom = $("#border-bottom")
+    var border_top = $("#border-top")
+    var border_right = $("#border-right")
+    var border_left = $("#border-left")
 
-    let start_ver = false
-
-    let player = $("#player")
-    let ball = $("#ball")
-    let border_bottom = $("#border-bottom")
-    let border_top = $("#border-top")
-    let border_right = $("#border-right")
-    let border_left = $("#border-left")
+    /** BOOLEANS THAT MAKE VERIFICATIONS IN THE CODE */
+    var start_ver = false
+    var pause = false;
 
     /** **************************************************/
 
 
     $("#ball").hide();
     player.hide();
+    oponent.hide();
 
     $("#start").click(function Innit(){
 
@@ -23,6 +26,7 @@ $(document).ready(() => {
 
         if (start_ver == true) {
             player.show()
+            oponent.show();
             $("#start").hide()
             ball.show();
 
@@ -30,6 +34,7 @@ $(document).ready(() => {
                 $("#field").mousemove((e) => {
                     // console.log(e.pageY)
                     playerMove(e.pageY)
+                    console.log(e.pageY)
                 })
             })
 
@@ -40,10 +45,20 @@ $(document).ready(() => {
     /** FUNCTIONS THAT WILL BE USED ALONG THE CODE */
     //! MOVE THE PLAYER SIDE
     function playerMove(y) {
-        if (y < border_bottom.position().top - 43) {
-            player.css({ top: (y - 43) + 'px' })
+        console.log("top: " + border_bottom.position().top)
+        console.log("y: " + y)
+
+        if((parseInt(y) + 2) > parseInt(border_bottom.position().left))
+        {
+            player.css({top: (y - 43)+'px'})
         }
     }
+    
+    /**
+     * That function was not made by me
+     * founded on stack overflow
+     */
+    //! FUNCTION THAT MAKES THE COLLISION
     function isOverlapping(div1, div2) {
         div1 = div1.getBoundingClientRect();
         div2 = div2.getBoundingClientRect();
@@ -55,64 +70,87 @@ $(document).ready(() => {
 
     //!MOVE THE BALL
     function ballMove() {
-        if (start_ver == true) {
+        if (start_ver == true && pause == false) {
             /**
-             * ? Variables that will define the direction that the ball will go 
+             * ? Variables that will define the direction that the ball will go
              */
-            var back_x = false;
-            var back_y = false;
-
-            var go = 'right';
+            let goX = 'left';
+            let goY = 'none'
+            
+            /**
+             *  ? Variables that will difine the inclination and speed of the ball
+             */
+            let inclination = 5;
+            //*The base speed is 5 pixels per repetition
+            let speed = 5;
             // ******************************
 
-
-
-            setInterval(() => {
+            let moveBall = setInterval(() => 
+            {
 
                 let ballPosition = ball.position();
 
                 let player = $("#player").position();
 
+                //set the player collision to make the ball return 
                 if (isOverlapping(document.querySelector("#player"), document.querySelector("#ball"))) {
-                    go = 'right'
+                    goX = 'right'
+                    
+                    speed = (speed < 20 ? speed + 2  : speed = 20)
                 }
 
-                switch (go) {
+                //set the oponent collision to make the ball return 
+                if (isOverlapping(document.querySelector("#oponent"), document.querySelector("#ball"))) {
+                    goX = 'left'
+
+                    speed = (speed < 20 ? speed + 2  : speed = 20)
+                }
+
+                //MAKES THE BALL MOVES IN THE X index
+                switch (goX) 
+                {
                     case 'right':
                         {
-                            if (ballPosition.left < border_right.position().left - 20) {
-                                ball.css({ left: '+=5px' })
+                            if(isOverlapping(document.querySelector("#ball"), document.querySelector("#border-right"))) 
+                            {
+                                goX = 'left'
                             }
-                            else { go = 'left' }
+                            else { ball.css({ left: '+='+speed+'px' }) }
                         }
                         break;
 
-                    case 'left':
+                        case 'left':
                         {
-                            if (ballPosition.left > border_left.position().left) {
-                                ball.css({ left: '-=5px' })
+                            if(isOverlapping(document.querySelector("#ball"), document.querySelector("#border-left"))) 
+                            {    
+                                goX = 'right'
                             }
-                            else { go = 'right' }
+                            else { ball.css({ left: '-='+speed+'px' }) }
                         }
                         break;
+                }
 
+                //MAKES THE BALL MOVES IN THE Y index
+                switch (goY) 
+                {
                     case 'top':
-                        {
-                            if (ballPosition.top > border_top.position().top) {
-                                ball.css({ top: '-=5px' })
-                            }
-                            else { go = 'bottom' }
+                    {
+                        if (isOverlapping(document.querySelector("#ball"), document.querySelector("#border-top"))) 
+                        {                            
+                            goY = 'bottom'
                         }
-                        break;
+                        else { ball.css({ top: '-='+inclination+'px' }) }
+                    }
+                    break;
 
                     case 'bottom':
-                        {
-                            if (ballPosition.top < border_bottom.position().top) {
-                                ball.css({ top: '+=5px' })
-                            }
-                            else { go = 'top' }
+                    {
+                        if (isOverlapping(document.querySelector("#ball"), document.querySelector("#border-bottom"))) {
+                            goY = 'top'
                         }
-                        break;
+                        else { ball.css({ top: '+='+inclination+'px' }) }
+                    }
+                    break;
                 }
 
             }, 10);
